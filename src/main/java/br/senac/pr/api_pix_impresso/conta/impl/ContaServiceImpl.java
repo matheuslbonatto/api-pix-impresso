573101cf15fc80f361ff70159781ede487610ec9
Matheus Luiz Bonatto <matheuslbonatto@gmail.com>
2024-07-08 19:09:47 -0300

    commit

--- src/main/java/br/senac/pr/api_pix_impresso/conta/impl/ContaServiceImpl.java
+++ src/main/java/br/senac/pr/api_pix_impresso/conta/impl/ContaServiceImpl.java
@@ -3,6 +3,7 @@ package br.senac.pr.api_pix_impresso.conta.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.senac.pr.api_pix_impresso.conta.ContaService;
@@ -48,7 +49,12 @@ public class ContaServiceImpl implements ContaService {
 
   @Override
   public DetailContaDto findById(Long id) {
-    return contaToDetailContaMapper.apply(contaRepository.findById(id).orElse(null));
+
+    var conta = contaRepository.findById(id).orElse(null);
+    if (conta == null) {
+      return null;
+    }
+    return contaToDetailContaMapper.apply(conta);
   }
 
   @Override
@@ -59,14 +65,15 @@ public class ContaServiceImpl implements ContaService {
 
   @Override
   public void deleteById(Long id) {
-    contaRepository.deleteById(id);
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
   }
 
   public DetailContaDto updateCadastro(Long id,
       UpdateContaCadastroDto dto) {
-
+    // Busca a conta
     Conta conta = contaRepository.findById(id).orElse(null);
-
+    // Valida se a conta existe
     if (conta == null) {
       throw new RuntimeException("Conta não encontrada");
     }
@@ -74,29 +81,6 @@ public class ContaServiceImpl implements ContaService {
     conta.setNome(dto.getNome());
     conta.setCpf(dto.getCpf());
     conta.setTipoConta(dto.getTipoConta());
-
-    contaRepository.update(conta);
-
-    return contaToDetailContaMapper.apply(conta);
-  }
-  public DetailContaDto update(Long id,
-      UpdateContaDto dto) {
-
-    Conta conta = contaRepository.findById(id).orElse(null);
-
-    if (conta == null) {
-      throw new RuntimeException("Conta não encontrada");
-    }
-
-    conta.setAgencia(dto.getAgencia());
-    conta.setNumeroConta(dto.getNumeroConta());
-    conta.setDigitoVerificador(dto.getDigitoVerificador());
-    conta.setNome(dto.getNome());
-    conta.setCpf(dto.getCpf());
-    conta.setTipoConta(dto.getTipoConta());
-    conta.setNumeroConta(dto.getNumeroConta());
-    conta.setSenha(dto.getSenha());
-    conta.setSaldo(dto.getSaldo());
 
     contaRepository.update(conta);
 
@@ -113,4 +97,36 @@ public class ContaServiceImpl implements ContaService {
     contaRepository.update(conta);
     return contaToDetailContaMapper.apply(conta);
   }
+
+  public Conta getByNumeroCartao(String numeroCartao) {
+    var conta = contaRepository.findByNumeroCartao(numeroCartao).orElse(null);
+    return conta;
+  }
+
+  public Conta efetuarDebito(String numeroCartao, String senha, Double valor) {
+    // Obter a conta
+    var conta = contaRepository.findByNumeroCartao(numeroCartao).orElse(null);
+    // validar se a conta foi encontrada
+    if (conta == null) {
+      throw new Error("Conta não encontrada");
+    }
+
+    // Validar a senha
+    // se a senha da conta não for igual a senha informada
+    if (!conta.getSenha().equals(senha)) {
+      throw new Error("Conta inválida");
+    }
+
+    // Validar o saldo
+    if (conta.getSaldo() < valor) {
+      throw new Error("Saldo da conta insuficiente");
+    }
+
+    // Efetuar o debito
+    conta.setSaldo(conta.getSaldo() - valor);
+
+    contaRepository.update(conta);
+    return conta;
+  }
+
 }
